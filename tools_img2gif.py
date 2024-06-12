ead36084836748e954de9351ebf1c5deb3072894
ChenyuZhangX <cyzhang21@mails.tsinghua.edu.cn>
2024-06-12 14:55:56 +0000

    gif converter created

--- tools_img2gif.py
+++ tools_img2gif.py
@@ -5,6 +5,21 @@ from PIL import Image
 import os
 
 path = 'outputs/toy_wo_clip_r4_ep10w/train/ours_100000/renders'
-names = [f"{idx:05d}.png" for idx in range(38, 100)]
-imgs = [Image.open(os.path.join(path, name)) for name in names]
-imgs[0].save('ours4.gif', save_all=True, append_images=imgs[1:], duration=300, loop=0)
+
+ranges = set(range(38, 90))
+to_del = set(range(73, 80))
+ranges = ranges.difference(to_del)
+
+print(ranges)
+
+
+names = [f"{idx:05d}.png" for idx in ranges]
+trans = Compose([
+    ToTensor(),
+])
+imgs = [trans(Image.open(os.path.join(path, name)).convert('RGB')) for name in names]
+
+
+
+imgs = [ToPILImage()(img) for img in imgs]
+imgs[0].save('workspace/ours.gif', save_all=True, append_images=imgs[1:], duration=100, loop=0)
